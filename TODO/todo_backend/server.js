20ff511aeeac3e684634c2a7fd5ef008e0700b81
Aqsa <aqsa2023123@gmail.com>
2026-05-12 19:34:37 +0000

    Working Docker Images code uploaded

--- TODO/todo_backend/server.js
+++ TODO/todo_backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/TODO',
+mongoose.connect('mongodb+srv://admin:admin123@punishmentmiffy.zm2x6s5.mongodb.net/?appName=punishmentmiffy',
     console.log('MongoDB connected')
 )
 
